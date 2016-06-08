5517b7dc133b33c8b96a47c6d532a72fddcccd75
AstridCordova <astridcordovar@gmail.com>
2016-06-08 16:23:26 -0500

    portafolio

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.chart').easyPieChart({
+    scaleColor: "#ecf0f1",
+    lineWidth: 10,
+    lineCap: 'butt',
+    barColor: '#C7C2CC',
+    trackColor:	"#eeecef",
+    size: 160,
+    animate: 2000
+  });
+});
